04d138c0718a0800d4ba671555581d0a42eb0687
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-08-04 21:51:18 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Maxim'
+print('Name: ' + name)
+age = 22
+print('Age: ' + str(age))
+new_age = age + 1
+print('New age: ' + str(new_age))
+is_student = True
+print('Is student: ' + str(is_student))

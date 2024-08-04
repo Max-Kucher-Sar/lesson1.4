61b7cf33cf43726ad87c2eab46f4b20dea1970f9
Max-Kucher-Sar <kucherenko.max75@mail.ru>
2024-08-04 22:13:59 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Maxim'
 print('Name: ' + name)
 age = 22
 print('Age: ' + str(age))
-new_age = age + 1
-print('New age: ' + str(new_age))
+age = age + 1
+print('New age: ' + str(age))
 is_student = True
 print('Is student: ' + str(is_student))
